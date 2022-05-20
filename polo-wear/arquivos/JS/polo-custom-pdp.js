7cb7a5e60b47e1becb379c4bbeb38d372af29d9c
j-dipierroSocial <j.dipierro@socialdigitalcommerce.com.br>
2022-05-20 17:52:40 -0300

    Escolha o Look

--- polo-wear/arquivos/JS/polo-custom-pdp.js
+++ polo-wear/arquivos/JS/polo-custom-pdp.js
@@ -17,8 +17,30 @@ var customPDP = {
             });
         });
     },
+    mandaLookAoCarrinho: function(){
+        $('.buy-all').click(function(){
+            var prodArray = new Array();
+            $(this).parent().find('li').each(function(){
+                var skuId = $(this).find('input.polo-sku').val();
+                prodArray.push({
+                    id: skuId,
+                    seller: 1,
+                    quantity: 1,
+                });
+            });
+            if (prodArray.length) {
+                vtexjs.checkout.addToCart(prodArray).done(function (a) {
+                    $(".header-minicart").addClass("open"),
+                    setTimeout(function () {
+                        $(".header-minicart").removeClass("open");
+                    }, 5e3);
+                });
+            }
+        });
+    },
     init: function(){
         customPDP.carregaImagemPDP();
+        customPDP.mandaLookAoCarrinho();
     }
 }
 customPDP.init();
