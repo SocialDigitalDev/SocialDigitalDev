96eda191dc3730f72105e2d4b022f9eae7de3c35
j-dipierroSocial <j.dipierro@socialdigitalcommerce.com.br>
2022-07-22 10:46:15 -0300

    Ajustamos o campo de frete para calcular corretamente

--- jasmine/src/js/jasmine-scripts.js
+++ jasmine/src/js/jasmine-scripts.js
@@ -926,16 +926,31 @@ $(document).ready(function () {
                         var atual = parseInt($(this).prev(".qtdVal").val());
                         //console.log(atual)
                         atual = atual + 1;
-                        $(this).prev(".qtdVal").val(atual);
+                        $(this).prev(".qtdVal").attr('value', `${atual}`);
+                        $('.calc-frete .cep .quantity input').attr('value', `${atual}`);
+						var cepValue = $('#txtCep').val().length;
+						if (cepValue !== 0) {
+							$('#btnFreteSimulacao').trigger('click')
+						}
                     });
 
                     $(".qtdPrateleira .btnDec").click(function () {
                         var atual = parseInt($(this).next(".qtdVal").val());
                         if (atual == 1) {
-                            $(this).next(".qtdVal").val("1");
+                            $(this).next(".qtdVal").attr('value', `1`);
+							$('.calc-frete .cep .quantity input').attr('value', `1`);
+							var cepValue = $('#txtCep').val().length;
+							if (cepValue !== 0) {
+								$('#btnFreteSimulacao').trigger('click')
+							}
                         } else {
                             atual = atual - 1;
-                            $(this).next(".qtdVal").val(atual);
+                            $(this).next(".qtdVal").attr('value', `${atual}`);
+							$('.calc-frete .cep .quantity input').attr('value', `${atual}`);
+							var cepValue = $('#txtCep').val().length;
+							if (cepValue !== 0) {
+								$('#btnFreteSimulacao').trigger('click')
+							}
                         }
                     });
 
