2b76c527f7f96d8b5983f9b78a438c49cbe96135
j-dipierroSocial <j.dipierro@socialdigitalcommerce.com.br>
2022-07-22 10:48:05 -0300

    Campo atualizado com nossa adaptação

--- uso-interno/ADDONS/Calcula CEP PDP/pdp-calcula-frete.js
+++ uso-interno/ADDONS/Calcula CEP PDP/pdp-calcula-frete.js
@@ -1,91 +1,41 @@
-"use strict";
-var calculacep = {
-    init: function() {
-        this.unavailableProduct()
-    },
-    //Calcula o frete na PDP
-    quantity: function() {
-        $(window);
-        $(document).on("click", ".js-quantity a", function(t) {
-            t.preventDefault();
-            var o = $(this)
-              , i = o.siblings(".quantity__field")
-              , n = parseInt(i.val()) + parseInt(o.data("number"))
-              , e = $(".form-product .form__actions .buy-button").attr("href").split("?")[1].split("&")
-              , c = []
-              , r = null;
-            n = n > 9 ? n : "" + n,
-            i.val(parseInt(n) ? n : "1");
-            for (var a in e)
-                "string" == typeof e[a] && e[a].indexOf("sku") > -1 && c.push(e[a].split("=").pop());
-            r = "/checkout/cart/add?sku=" + c + "&qty=" + n + "&seller=1&redirect=true&sc=1",
-            $(".form-product .form__actions .buy-button").attr("href", r);
-            $('#calculoFrete .quantity input').val(n).attr('value', n);
+if ("abc" == "abc") {
+    setTimeout(function () {
+        $(".qtdPrateleira .btnAcr").click(function () {
+            var atual = parseInt($(this).prev(".qtdVal").val());
+            //console.log(atual)
+            atual = atual + 1;
+            $(this).prev(".qtdVal").attr('value', `${atual}`);
+            $('.calc-frete .cep .quantity input').attr('value', `${atual}`);
             var cepValue = $('#txtCep').val().length;
-            console.log('valor:' + cepValue)
             if (cepValue !== 0) {
                 $('#btnFreteSimulacao').trigger('click')
             }
-        })
-    },
-    //Quando o produto está indisponível
-    unavailableProduct: function() {
-        vtexjs.catalog.getProductWithVariations(skuJson_0.productId).done(function(t) {
-            if (t.available) {
-                $(".form-product .js-quantity, #produtoDiv-direita .plugin-preco").show()
+        });
+
+        $(".qtdPrateleira .btnDec").click(function () {
+            var atual = parseInt($(this).next(".qtdVal").val());
+            if (atual == 1) {
+                $(this).next(".qtdVal").attr('value', `1`);
+                $('.calc-frete .cep .quantity input').attr('value', `1`);
+                var cepValue = $('#txtCep').val().length;
+                if (cepValue !== 0) {
+                    $('#btnFreteSimulacao').trigger('click')
+                }
             } else {
-                $(".form-product .js-quantity, #produtoDiv-direita .plugin-preco").remove();
-                $.getJSON('https://api.vtex.com/jasmine/pricing/prices/' + t.skus[0].sku).done(function(json) {
-                    var o = JSON.parse(json);
-                    console.log(o);
-                    var basePrice = o.basePrice;
-                    var listPrice = o.listPrice;
-                    var valParcelaMinima = 20.00;
-                    var numParcelaMaxima = 12;
-                    var numParcelas = parseInt(basePrice / valParcelaMinima);
-                    numParcelas = (numParcelas > numParcelaMaxima) ? numParcelaMaxima : numParcelas;
-                    var valParcelas = (basePrice / numParcelas);
-                    var valorAvista = basePrice - (basePrice * 0.1);
-                    basePrice = basePrice.toLocaleString("pt-BR", {
-                        style: 'currency',
-                        currency: 'BRL',
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2
-                    });
-                    listPrice = listPrice.toLocaleString("pt-BR", {
-                        style: 'currency',
-                        currency: 'BRL',
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2
-                    });
-                    valorAvista = valorAvista.toLocaleString("pt-BR", {
-                        style: 'currency',
-                        currency: 'BRL',
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2
-                    });
-                    valParcelas = valParcelas.toLocaleString("pt-BR", {
-                        style: 'currency',
-                        currency: 'BRL',
-                        maximumFractionDigits: 2,
-                        minimumFractionDigits: 2
-                    });
-                    
-                    $(".product__content .product__price .sku__price").show();
-                    $(".product__content .product__price").show()
-                }).fail(function(a, b, c) {
-                    console.error(a);
-                    console.error(b);
-                    console.error(c)
-                })
+                atual = atual - 1;
+                $(this).next(".qtdVal").attr('value', `${atual}`);
+                $('.calc-frete .cep .quantity input').attr('value', `${atual}`);
+                var cepValue = $('#txtCep').val().length;
+                if (cepValue !== 0) {
+                    $('#btnFreteSimulacao').trigger('click')
+                }
             }
-        })
-    }
-};
-calculacep.init(),
-$(document).ready(function() {
-    ShippingValue();//Controle da VTEX padrão
-})
-$(window).on('load', function() {
-    calculacep.quantity();   
-})
+        });
+
+        $(".qtdPrateleira .qtdVal").bind("keyup blur focus", function (e) {
+            e.preventDefault();
+            var expre = /[^\d]/g;
+            $(this).val($(this).val().replace(expre, ''));
+        });
+    }, 200);
+}
